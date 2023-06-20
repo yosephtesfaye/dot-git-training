8c3b4a7f8caebca5dd36af2d0379fb6cef7d63d9
yosephtesfaye <tewdemam@gmail.com>
2023-06-20 08:55:18 +0300

    project1

---
+++ Personal Introduction Page/code/script.js
@@ -0,0 +1,14 @@
+// Get the h1 element
+var heading = document.getElementById('welcomeMessage');
+// Set the text content
+heading.textContent = 'Welcome to my Introduction Page';
+
+// let person = prompt("Please enter your name");
+
+// if (person != null) {
+//   document.getElementById("name").innerHTML =
+//    person;
+// }
+
+let person = prompt("enter your name");
+document.getElementById("name").innerHTML=person;
